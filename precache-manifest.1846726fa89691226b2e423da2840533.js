0022d3050089f0dfc53651c62da49bc8553243bc
Santos Gonzalez <sgonzo3@gmail.com>
2019-03-26 12:51:19 -0400

    Updates

---
+++ precache-manifest.1846726fa89691226b2e423da2840533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d10525dbbd17c8c9c86",
+    "url": "/Neighborhood-Map-Geneva/static/js/runtime~main.3fe93837.js"
+  },
+  {
+    "revision": "c07cd16b10bc7ac25297",
+    "url": "/Neighborhood-Map-Geneva/static/js/main.d3229b55.chunk.js"
+  },
+  {
+    "revision": "98bcaedc0ba08152c518",
+    "url": "/Neighborhood-Map-Geneva/static/js/2.a1077f6f.chunk.js"
+  },
+  {
+    "revision": "c07cd16b10bc7ac25297",
+    "url": "/Neighborhood-Map-Geneva/static/css/main.b140e3ec.chunk.css"
+  },
+  {
+    "revision": "5f0d5b119ea0f8fd37eaabcb88dbbc55",
+    "url": "/Neighborhood-Map-Geneva/index.html"
+  }
+];
